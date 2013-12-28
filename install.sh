c0a40d8b09dd1f38bc978e754edae3afe0b1da5f
Marc-Antoine Courteau <macourteau@users.noreply.github.com>
2013-12-28 17:56:20 -0500

    Imported gitconfig and updated install.sh for it.

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-cp -v .profile ~/.profile
-cp -v .screenrc ~/.screenrc
-cp -v .vimrc ~/.screenrc
+cp -v gitconfig ~/.gitconfig
+cp -v profile ~/.profile
+cp -v screenrc ~/.screenrc
+cp -v vimrc ~/.screenrc
+
+read -p "Git user name: " git_user_name
+read -p "Git user email: " git_user_email
+
+git config --global user.name "$git_user_name"
+git config --global user.email "$git_user_email"
