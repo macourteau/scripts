0c5e4cc60a834c617c9552fbd17038b44896c05d
Marc-Antoine Courteau <macourteau@users.noreply.github.com>
2013-12-28 18:03:29 -0500

    No longer ask for git user name/email if it was already setup.

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
 
+git_user_name=$(git config --global user.name || echo -n)
+git_user_email=$(git config --global user.email || echo -n)
+
 cp -v gitconfig ~/.gitconfig
 cp -v profile ~/.profile
 cp -v screenrc ~/.screenrc
 cp -v vimrc ~/.screenrc
 
-read -p "Git user name: " git_user_name
-read -p "Git user email: " git_user_email
+if [[ "$git_user_name" == "" ]]; then
+  read -p "Git user name: " git_user_name
+fi
+
+if [[ "$git_user_email" == "" ]]; then
+  read -p "Git user email: " git_user_email
+fi
 
 git config --global user.name "$git_user_name"
 git config --global user.email "$git_user_email"
