7a5b496aae1a85db3b7838a1703d74cc0e4f1a48
Marc-Antoine Courteau <macourteau@absurdsoftware.com>
2014-03-13 11:44:27 -0400

    Adds a basic .ssh/config file.

--- install.sh
+++ install.sh
@@ -3,10 +3,20 @@
 git_user_name=$(git config --global user.name || echo -n)
 git_user_email=$(git config --global user.email || echo -n)
 
+if [[ ! -d ~/.ssh ]]
+then
+  mkdir ~/.ssh
+  chmod 700 ~/.ssh
+fi
+
 cp -v gitconfig ~/.gitconfig
 cp -v profile ~/.profile
 cp -v screenrc ~/.screenrc
 cp -v vimrc ~/.vimrc
+if [[ ! -e ~/.ssh/config ]]
+then
+  cp -v sshconfig ~/.ssh/config
+fi
 
 if [[ "$git_user_name" == "" ]]; then
   read -p "Git user name: " git_user_name
