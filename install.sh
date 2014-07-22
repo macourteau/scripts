3a52fa29d15666e971fefd0ee285075292f3eab4
Marc-Antoine Courteau <macourteau@absurdsoftware.com>
2014-07-22 16:18:01 -0400

    Improve install script.

--- install.sh
+++ install.sh
@@ -3,25 +3,24 @@
 git_user_name=$(git config --global user.name || echo -n)
 git_user_email=$(git config --global user.email || echo -n)
 
-if [[ ! -d $HOME/.ssh ]]
-then
-  mkdir $HOME/.ssh
+if [[ ! -d $HOME/.ssh ]]; then
+  mkdir -p $HOME/.ssh
   chmod 700 $HOME/.ssh
 fi
 
-cp -v gitconfig $HOME/.gitconfig
-cp -v screenrc $HOME/.screenrc
-cp -v vimrc $HOME/.vimrc
-cp -v tmux.conf $HOME/.tmux.conf
+function install-dotfile() {
+  cp -v "$1" "${HOME}/.$1" && chmod 644 "${HOME}/.$1"
+}
 
-# Vim colorscheme.
-git clone https://github.com/nanotech/jellybeans.vim $HOME/.vim
+install-dotfile gitconfig
+install-dotfile screenrc
+install-dotfile vimrc
+install-dotfile tmux.conf
 
-cp -v profile $HOME/.profile
+install-dotfile profile
 mkdir -p $HOME/.profile.d
 
-if [[ ! -e $HOME/.ssh/config ]]
-then
+if [[ ! -e $HOME/.ssh/config ]]; then
   cp -v sshconfig $HOME/.ssh/config
 fi
 
@@ -29,19 +28,22 @@ if [[ "x$(uname)" == "xDarwin" ]]; then
   cat screenrc | grep -v deflogin > $HOME/.screenrc
 fi
 
-if [[ "$git_user_name" == "" ]]; then
+if [[ "x${git_user_name}" == "x" ]]; then
   read -p "Git user name: " git_user_name
 fi
 
-if [[ "$git_user_email" == "" ]]; then
+if [[ "x${git_user_email}" == "x" ]]; then
   read -p "Git user email: " git_user_email
 fi
 
 git config --global user.name "$git_user_name"
 git config --global user.email "$git_user_email"
 
+# Vim colorscheme.
+[[ -d $HOME/.vim ]] || git clone https://github.com/nanotech/jellybeans.vim $HOME/.vim
+
 function install-binary() {
-  cp -v "bin/$1" "${HOME}/bin" && chmod +x "${HOME}/bin/$1"
+  cp -v "bin/$1" "${HOME}/bin" && chmod 755 "${HOME}/bin/$1"
 }
 
 mkdir -p $HOME/bin
